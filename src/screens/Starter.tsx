99da2b88288405078b89957e9efb37e19efab604
Arthur Monney <monneylobe@gmail.com>
2023-03-22 22:02:02 +0100

    :wheelchair: remove dark mode

--- src/screens/Starter.tsx
+++ src/screens/Starter.tsx
@@ -12,56 +12,18 @@ import translate, { LocaleContext } from '@/utils/i18n'
 import { ThemeContext } from '@/utils/theme'
 import { LOCALES } from '@/utils/constants'
 import { IntroCard } from '@/components/Cards'
-import colors from '@/utils/colors'
-import {
-  Redux,
-  Tailwind,
-  Icon,
-  Moon,
-  Sun,
-  Cypress,
-  Eslint,
-} from '@components/Svg'
+import { Redux, Tailwind, Icon, Cypress, Eslint } from '@components/Svg'
 
 const Starter = () => {
   const tailwind = useTailwind()
   const { locale, changeLocale } = React.useContext(LocaleContext)
-  const { theme, toggleTheme } = React.useContext(ThemeContext)
+  const { theme } = React.useContext(ThemeContext)
 
   return (
     <SafeAreaView
       style={{ flex: 1, ...tailwind('bg-white dark:bg-slate-900') }}
     >
       <StatusBar style={theme === 'light' ? 'dark' : 'light'} />
-      <View style={tailwind('absolute top-14 right-4')}>
-        <TouchableOpacity
-          style={{
-            ...tailwind(
-              'relative bg-white border border-slate-200 dark:border-transparent dark:bg-slate-800 py-2 px-3 rounded-md'
-            ),
-            shadowColor: colors.slate[400],
-            shadowOffset: {
-              width: 1,
-              height: 2,
-            },
-          }}
-          onPress={toggleTheme}
-        >
-          {theme === 'light' ? (
-            <View style={tailwind('flex-row items-center')}>
-              <Sun width={20} height={20} fill={colors.yellow[400]} />
-              <Text style={tailwind('text-base text-slate-600 ml-2')}>
-                Clair
-              </Text>
-            </View>
-          ) : (
-            <View style={tailwind('flex-row items-center')}>
-              <Moon width={24} height={24} fill={colors.white} />
-              <Text style={tailwind('text-base text-white ml-2')}>Sombre</Text>
-            </View>
-          )}
-        </TouchableOpacity>
-      </View>
       <ScrollView style={{ paddingHorizontal: 24 }}>
         <View style={tailwind('pt-20 items-center justify-center h-full')}>
           <Icon width={60} height={60} />
